28eabdf92690142c2919304b402a762db3720e48
CuongNguyen2000 <cn1122000@gmail.com>
2021-10-15 09:21:46 +0700

    import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { AppointmentsModule } from './appointments/appointments.module';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UsersModule, AppointmentsModule, LoggerModule],
   controllers: [],
   providers: [],
 })
