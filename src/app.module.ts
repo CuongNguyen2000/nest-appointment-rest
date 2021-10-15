eadb9e2e707f29c14ec2aa34e649956825f3ca01
CuongNguyen2000 <cn1122000@gmail.com>
2021-10-15 09:25:52 +0700

    update with lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AppointmentsModule } from './appointments/appointments.module';
 import { LoggerModule } from './logger/logger.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AppointmentsModule, LoggerModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    PrismaModule,
+    UsersModule,
+    AppointmentsModule,
+    LoggerModule,
+  ],
   controllers: [],
   providers: [],
 })
